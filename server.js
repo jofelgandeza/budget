259d66963ea3980ee2e1db45e46d6ef3b0601117
Jofel Gandezam <jofelgandeza@yahoo.com>
2021-08-03 17:50:11 +0800

    Modified mongoose.connect

--- server.js
+++ server.js
@@ -42,7 +42,8 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+    useNewUrlParser: true, useUnifiedTopology: true})
+
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongo Database'))
